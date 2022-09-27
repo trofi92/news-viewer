f2a05f7b01047fa039cdc95be8d821739d86b4da
lunaslash9249@gmail.com <lunaslash9249@gmail.com>
2022-09-27 14:55:04 +0900

    react-news-api

---
+++ src/pages/NewsPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Categories from "../components/Categories";
+import NewsList from "../components/NewsList";
+import { useParams } from "react-router-dom";
+
+const NewsPage = () => {
+  const params = useParams();
+  // 카테고리가 선택되지 않았으면 기본값 all로 사용
+  const category = params.category || "all";
+
+  return (
+    <>
+      <Categories />
+      <NewsList category={category} />
+    </>
+  );
+};
+
+export default NewsPage;
